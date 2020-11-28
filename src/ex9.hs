23901160defd897a4a28e5ae390b6d1ca907ea9b
Matt Kaye <mrkaye97@gmail.com>
2020-11-28 14:46:13 -0500

    some print improvements

--- src/ex9.hs
+++ src/ex9.hs
@@ -1,3 +1,13 @@
+-- getting pythagorean triples is simple
+-- if we have 2 integers m and n such that m > n,
+-- then we can find a pythagorean triple (a,b,c)
+-- s.t. a = m^2 - n^2, b = 2 * m * n, c = m^2 + n^2
+-- thus, my approach is to first get the cartestian product of
+-- M = [1,2..1000], N = [1,2..1000]
+-- and then  filter than list down s.t. m > n
+-- and then get the pythagorean triple corresponding to that (m,n) pair
+-- and finally, to check if a + b + c == 1000
+
 cartProd :: [Int] -> [Int] -> [(Int, Int)]
 cartProd a b = [ (x,y) | x <- a, y <- b ]
 
@@ -6,7 +16,6 @@ mgrn (m,n) = if m > n
   then True
   else False
 
--- getting pythagorean triples is simple, if we have 2 integers m and n such that m > n
 canWork :: (Int, Int) -> Bool
 canWork (m,n) = if (m + n < 1000) &&
   (((getA (m, n)) + (getB (m,n)) + (getC (m,n))) == 1000)
@@ -23,9 +32,11 @@ getC :: (Int, Int) -> Int
 getC (m, n) = m^2 + n^2
 
 main = do
-  let x = (filter canWork (filter mgrn (cartProd [1,2..1000] [1,2..1000])))
-  let a = (map getA x) !! 0
-  let b = (map getB x) !! 0
-  let c = (map getC x) !! 0
+  let x = (filter canWork (filter mgrn (cartProd [1,2..1000] [1,2..1000]))) !! 0
+  let a = getA x
+  let b = getB x
+  let c = getC x
   print(a, b, c)
-  print(a + b + c)
+  putStrLn $ "Triple = " ++ show (a,b,c)
+  putStrLn $ "Sum = " ++ show (a+b+c)
+  putStrLn $ "Product = " ++ show (a*b*c)
